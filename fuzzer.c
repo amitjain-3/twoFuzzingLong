9d93fc909c445dd2a57b24bc02808570fde5dd6f
Dhruv Swarup <dhruvswarup123@gmail.com>
2021-11-20 18:48:47 -0800

    fix main bugs

--- fuzzer.c
+++ fuzzer.c
@@ -1,8 +1,11 @@
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sched.h>
 #ifdef __APPLE__
     #include <mach/thread_act.h>
 #endif
@@ -10,7 +13,6 @@
 #include "include/node.h"
 #include "include/runtime_stats.h"
 
-#define _GNU_SOURCE
 #define NUM_THREADS 8
 #define NUM_MUTATION_FUNCS 2
 
@@ -52,23 +54,6 @@ int determineCoreCount(){
 }
 
 
-void add(int num, int runtime){
-    Node * node = malloc(sizeof(Node));
-    node->runtime = runtime; 
-    node->id = num;
-    queue_sorted_put(node);
-}
-
-
-void rem(){
-    Node * prev;
-    queue_get(&prev);
-    if (prev == NULL) return;
-    node_print(prev); printf("\n");
-    free(prev);
-}
-
-
 void interesting_inputs_to_queue(char * filename){
     FILE * fp = fopen(filename, "r");
     char buf[INPUT_SIZE] = {0};
@@ -99,17 +84,17 @@ void * fuzz_loop(){
     int i = 0;
     while (i++ < 10){
         // Get one input from queue
-        queue_get(&curr);
+        // queue_get(&curr);
 
         // Mutate it
-        mutate(curr->input);
+        // mutate(curr->input);
 
         // Check if interesting and add
 
-        free(curr); 
+        // free(curr); 
     }
 
-    pthread_exit(NULL);
+    return NULL;
 }
 
 
@@ -142,7 +127,7 @@ int main(){
 
         // Set the affinity of thread to core
         pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cpus);
-        rc = pthread_create(&threads[i], &attr, fuzz_loop, (void *)i);
+        int rc = pthread_create(&threads[i], &attr, fuzz_loop, NULL);
         if (rc) {
             printf("Error:unable to create thread, %d\n", rc);
             exit(-1);
@@ -160,96 +145,97 @@ int main(){
     // Destroy queue stuff
     avada_Qdavra();
     pthread_mutex_destroy(&qlock);
+    pthread_attr_destroy(&attr);
     
     return 0;
 }
 
 
-void some_old_main_stuff(){
-    int processorCount = 1; //default 
-    pthread_attr_t *affinity_attr = NULL; 
+// void some_old_main_stuff(){
+//     int processorCount = 1; //default 
+//     pthread_attr_t *affinity_attr = NULL; 
 
-    processorCount = sysconf(_SC_NPROCESSORS_ONLN);
-    printf("Number of logical cores: %d\n", processorCount);
+//     processorCount = sysconf(_SC_NPROCESSORS_ONLN);
+//     printf("Number of logical cores: %d\n", processorCount);
 
 
-#ifdef __APPLE__
-    /*need to figure out how to set affinity, cpu_set_t does not exist. 
-      doesnt look like we can set affinity explictly on OSX, 
-      but can give hints to kernel which threads do not need to share L2 cache so that they can be scheduled appropriate */
-    printf("Still working on this \n");
-#elif __linux__
-    pthread_attr_t attr;
-    cpu_set_t cpus;
-    pthread_attr_init(&attr);
-    printf("initialized attribute \n");
-#endif
+// #ifdef __APPLE__
+//     /*need to figure out how to set affinity, cpu_set_t does not exist. 
+//       doesnt look like we can set affinity explictly on OSX, 
+//       but can give hints to kernel which threads do not need to share L2 cache so that they can be scheduled appropriate */
+//     printf("Still working on this \n");
+// #elif __linux__
+//     pthread_attr_t attr;
+//     cpu_set_t cpus;
+//     pthread_attr_init(&attr);
+//     printf("initialized attribute \n");
+// #endif
     
-    pthread_t threads[processorCount];
+//     pthread_t threads[processorCount];
 
-    int rc;
-    int i;
-    for( i = 0; i < processorCount; i++ ) {
-        //assign cpu mask here in cpus and use set affinity passing cpu to create attribute, pass attr to pthread_create on each loop (works for linux - need different way for OSX/unix)
-#ifdef __linux__
-        CPU_ZERO(&cpus);
-        CPU_SET(i, &cpus);
-        pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cpus);
-        affinity_attr = &attr;
-#endif
-        rc = pthread_create(&threads[i], affinity_attr, add, (void *)i);
-        if (rc) {
-            printf("Error:unable to create thread, %d\n", rc);
-            exit(-1);
-        }
-        //usleep(250000); 
-    }
+//     int rc;
+//     int i;
+//     for( i = 0; i < processorCount; i++ ) {
+//         //assign cpu mask here in cpus and use set affinity passing cpu to create attribute, pass attr to pthread_create on each loop (works for linux - need different way for OSX/unix)
+// #ifdef __linux__
+//         CPU_ZERO(&cpus);
+//         CPU_SET(i, &cpus);
+//         pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cpus);
+//         affinity_attr = &attr;
+// #endif
+//         rc = pthread_create(&threads[i], affinity_attr, add, (void *)i);
+//         if (rc) {
+//             printf("Error:unable to create thread, %d\n", rc);
+//             exit(-1);
+//         }
+//         //usleep(250000); 
+//     }
 
-    for( i = 0; i < processorCount; i++ ) {
-        pthread_join(threads[i], NULL);
-    }
+//     for( i = 0; i < processorCount; i++ ) {
+//         pthread_join(threads[i], NULL);
+//     }
 
-    for( i = 0; i < processorCount-3; i++ ) {
-#ifdef __linux__
-        CPU_ZERO(&cpus);
-        CPU_SET(i, &cpus);
-        pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cpus);
-        affinity_attr = &attr;
-#endif
-        rc = pthread_create(&threads[i], affinity_attr, rem, NULL);
-        if (rc) {
-            printf("Error:unable to create thread, %d\n", rc);
-            exit(-1);
-        }
-        //usleep(250000);
-    }
+//     for( i = 0; i < processorCount-3; i++ ) {
+// #ifdef __linux__
+//         CPU_ZERO(&cpus);
+//         CPU_SET(i, &cpus);
+//         pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cpus);
+//         affinity_attr = &attr;
+// #endif
+//         rc = pthread_create(&threads[i], affinity_attr, rem, NULL);
+//         if (rc) {
+//             printf("Error:unable to create thread, %d\n", rc);
+//             exit(-1);
+//         }
+//         //usleep(250000);
+//     }
 
-    for( i = 0; i < processorCount-3; i++ ) {
-        pthread_join(threads[i], NULL);
-    }
+//     for( i = 0; i < processorCount-3; i++ ) {
+//         pthread_join(threads[i], NULL);
+//     }
 
-    queue_print();
-    avada_Qdavra();
+//     queue_print();
+//     avada_Qdavra();
     
-    // Execution time Test
-    /*double time_spent = 0.0;
-    clock_t begin = clock();
-    // do some stuff here
-    unsigned char input[100] = "000000000000321000000000000000000000000000000000000000000000006540000000000000000000000000000000000\n";
-    //selection_sort(input);
-    sleep(3);
-    clock_t end = clock();
-    // calculate elapsed time by finding difference (end - begin) and
-    // dividing the difference by CLOCKS_PER_SEC to convert to seconds
-    time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
-    printf("The elapsed time is %f seconds \n", time_spent);
-    printf("The clocks elapsed is %lu \n", (end-begin));*/
+//     // Execution time Test
+//     /*double time_spent = 0.0;
+//     clock_t begin = clock();
+//     // do some stuff here
+//     unsigned char input[100] = "000000000000321000000000000000000000000000000000000000000000006540000000000000000000000000000000000\n";
+//     //selection_sort(input);
+//     sleep(3);
+//     clock_t end = clock();
+//     // calculate elapsed time by finding difference (end - begin) and
+//     // dividing the difference by CLOCKS_PER_SEC to convert to seconds
+//     time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+//     printf("The elapsed time is %f seconds \n", time_spent);
+//     printf("The clocks elapsed is %lu \n", (end-begin));*/
 
-    pthread_mutex_destroy(&qlock);
-    pthread_exit(NULL);
+//     pthread_mutex_destroy(&qlock);
+//     pthread_exit(NULL);
 
-    return 0;
-}
+//     return 0;
+// }
 
 // Write mutate func
 // Store interesting inputs in a queue
