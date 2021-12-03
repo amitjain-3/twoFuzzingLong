37f1b7981daa9d397cf39dab64dd4e65c13c289d
Amit <amitrjain11@gmail.com>
2021-12-03 15:24:43 -0800

    more 2 core data

--- scripts/test_parallel.sh
+++ scripts/test_parallel.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 rm data/temp.txt
 
-for i in $(seq 80)
+for i in $(seq 120)
 do
    #./scripts/multiprocessing.sh >> data/temp.txt
-   ./all coverage >> data/4_core_out_multithread_2.csv
+   ./all coverage >> data/2_core_out_multithread_2.csv
 
 done
 
