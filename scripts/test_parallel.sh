6184685e4443c2fd31a707e774213e79bc767b70
Amit <amitrjain11@gmail.com>
2021-12-03 14:16:38 -0800

    more 4 core data

--- scripts/test_parallel.sh
+++ scripts/test_parallel.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 rm data/temp.txt
 
-for i in $(seq 10)
+for i in $(seq 80)
 do
-   ./scripts/multiprocessing.sh >> data/temp.txt
-   #./all coverage >> data/4_core_out_multiprocess.csv
+   #./scripts/multiprocessing.sh >> data/temp.txt
+   ./all coverage >> data/4_core_out_multithread_2.csv
 
 done
 
-python3 scripts/filter.py > data/2_core_out_multiprocess.csv
+#python3 scripts/filter.py > data/2_core_out_multiprocess.csv
